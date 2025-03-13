b583396d47e31ebd62c1b97da5bf86045e1aebaa
Suppi-123 <supriyakeshav72@gmail.com>
2025-03-13 10:29:06 +0530

    overall backend

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from .database.connection import connect_to_db
 from .routes import hr_routes, finance_routes, master_order_routes
 from .api.v1.endpoints import document_management, inventoryv1, priority_scheduling
 from .api.v1.endpoints import component_status, programs, daily_production, operator_login
-from .api.v1.endpoints import auth, planning, mpp, operations, scheduled, dynamic_rescheduling, comp_maintainance, comp_operator, document_management_v2, production_monitoring
+from .api.v1.endpoints import auth, planning, mpp, operations, scheduled, dynamic_rescheduling, comp_maintainance, comp_operator, document_management_v2, production_monitoring, production_logs
 
 
 app = FastAPI(title="BEL MES API")
@@ -41,6 +41,7 @@ app.include_router(comp_maintainance.router)
 app.include_router(comp_operator.router)
 app.include_router(component_status.router)
 app.include_router(scheduled.router)
+app.include_router(production_logs.router)
 app.include_router(priority_scheduling.router)
 app.include_router(dynamic_rescheduling.router)
 app.include_router(programs.router)
@@ -61,3 +62,4 @@ def read_root():
 
 # uvicorn app.main:app --reload
 # uvicorn app.main:app --host 172.18.7.85 --port 6797 --reload
+# uvicorn app.main:app --host 172.18.7.88 --port 6699 --reload
