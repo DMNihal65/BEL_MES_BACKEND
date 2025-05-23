eb4001dc969b95db54bcdf374aa78677521ae3bb
Suppi-123 <supriyakeshav72@gmail.com>
2025-05-23 17:07:35 +0530

    changes made in  part production timeline

--- app/main.py
+++ app/main.py
@@ -74,11 +74,11 @@ def read_root():
 
 # uvicorn app.main:app --reload
 # uvicorn app.main:app --host 172.18.7.88 --port 4476 --reload
-# uvicorn app.main:app --host 172.18.7.88 --port 6677 --reload
+# uvicorn app.main:app --host 172.18.7.88 --port 6574 --reload
 # uvicorn app.main:app --host 172.18.7.88 --port 5479 --reload
 # uvicorn app.main:app --host 172.18.7.88 --port 4377 --reload
 
-# uvicorn app.main:app --host 172.18.7.88 --port 5464 --reload
+# uvicorn app.main:app --host 172.18.7.88 --port 5554 --reload
 
 
 # uvicorn app.main:app --host 172.18.7.88 --port 5598 --reload
