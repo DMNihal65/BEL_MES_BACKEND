c050b0ddf54bc60d15dd3f12a35f1e6f43fe3214
D M Nihal <73550295+DMNihal65@users.noreply.github.com>
2025-10-17 08:50:05 +0530

    updated backend 17-10-25

--- server-docker-setup.sh
+++ server-docker-setup.sh
@@ -14,12 +14,12 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 # Configuration (can be overridden with environment variables)
-IMAGE_NAME=${IMAGE_NAME:-"bel-fastapi-app-v13.1.2-20251007-171123"}
-CONTAINER_NAME=${CONTAINER_NAME:-"bel-fastapi-v13.1.2-20251007-171123"}
-PORT=${PORT:-"8002"}
+IMAGE_NAME=${IMAGE_NAME:-"bel-fastapi-app-v15.3.0-20251016-110218"}
+CONTAINER_NAME=${CONTAINER_NAME:-"bel-fastapi-v15.3.0-20251016-110218"}
+PORT=${PORT:-"8008"}
 BACKUP_DIR=${BACKUP_DIR:-"/home/smc/bel/backups"}
 CURRENT_DIR=${CURRENT_DIR:-"/home/smc/bel/current"}
-IMAGE_TAR=${IMAGE_TAR:-"bel-fastapi-app-v13.1.2-20251007-171123.tar"}
+IMAGE_TAR=${IMAGE_TAR:-"bel-fastapi-app-v15.3.0-20251016-110218.tar"}
 
 # Print banner
 echo -e "${BLUE}============================================${NC}"
